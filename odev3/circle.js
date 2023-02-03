919f405e3ccbbbd1ff4420840ff0259acfef1511
Mehmet ErdemVURAL <mehmeterdemvural@gmail.com>
2023-02-04 00:56:46 +0300

    completed odev3

---
+++ odev3/circle.js
@@ -0,0 +1,9 @@
+const area = (r) => {
+  console.log("Circle area: ", `${r * r} π`);
+};
+
+const circleCircumference = (r) => {
+  console.log("Circle circleCircumference: ", `${2 * r} π`);
+};
+
+export { area, circleCircumference };
---
+++ odev3/index.js
@@ -0,0 +1,4 @@
+import { circleCircumference, area } from "./circle.js";
+
+circleCircumference(5);
+area(5);
