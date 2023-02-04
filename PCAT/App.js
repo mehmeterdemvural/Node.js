2268393952c2bcaf00b5160f88654c9201514272
Mehmet ErdemVURAL <mehmeterdemvural@gmail.com>
2023-02-05 00:55:53 +0300

    created CleanBlog Folder

--- PCAT/App.js
+++ PCAT/App.js
@@ -1,1 +1,13 @@
-console.log('Merhaba');
+import express from 'express';
+const app = express();
+
+
+app.get("/", (req, res)=> {
+    res.send("Merhaba");
+})
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu port ${port} de çalışmaya başladı ! !`);
+});
+ 
