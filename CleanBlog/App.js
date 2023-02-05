a8ce2177a85705a43f8b1fd03d0da40f064373b2
Mehmet Erdem VURAL <mehmeterdemvural@gmail.com>
2023-02-05 22:48:19 +0300

    added partials

--- CleanBlog/App.js
+++ CleanBlog/App.js
@@ -1,13 +1,26 @@
 import express from 'express';
+import ejs from 'ejs';
+
 const app = express();
 
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
-  res.send(blog);
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/post', (req, res) => {
+  res.render('post');
+});
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
 });
 
 const port = 3000;
 app.listen(port, () => {
   console.log(`Sunucu port ${port} de çalışmaya başladı ! !`);
 });
- 
