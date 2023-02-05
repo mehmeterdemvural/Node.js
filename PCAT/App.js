29974554315a1c4e1dd1b360020cd8ae1c3f64f2
Mehmet ErdemVURAL <mehmeterdemvural@gmail.com>
2023-02-05 14:01:53 +0300

    added partials

--- PCAT/App.js
+++ PCAT/App.js
@@ -1,13 +1,23 @@
 import express from 'express';
+import ejs from 'ejs';
+
 const app = express();
 
+app.set('view engine', 'ejs');
 
-app.get("/", (req, res)=> {
-    res.send("Merhaba");
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index')
+});
+app.get('/about', (req, res) => {
+  res.render('about')
+});
+app.get('/add', (req, res) => {
+  res.render('add')
 });
 
 const port = 3000;
 app.listen(port, () => {
   console.log(`Sunucu port ${port} de çalışmaya başladı ! !`);
 });
- 
