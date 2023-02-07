7b6d7b9fb42b96142c043aea9138591d7debaff3
Mehmet Erdem VURAL <mehmeterdemvural@gmail.com>
2023-02-07 14:56:12 +0300

    completed odev9

--- CleanBlog/App.js
+++ CleanBlog/App.js
@@ -1,14 +1,31 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import ejs from 'ejs';
 
+import { Post } from './models/Post.js';
+
 const app = express();
+mongoose.set('strictQuery', false);
+
+mongoose.connect('mongodb://127.0.0.1:27017/pcat-test-db', (err, res) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log('DB Bağlantı Başarılı');
+});
 
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const posts = await Post.find({});
+  console.log(posts[0]);
+  res.render('index', {
+    posts,
+  });
 });
 app.get('/about', (req, res) => {
   res.render('about');
@@ -18,6 +35,16 @@ app.get('/post', (req, res) => {
 });
 app.get('/add_post', (req, res) => {
   res.render('add_post');
+});
+
+app.post('/post', async (req, res) => {
+  await Post.create(req.body, (err, post) => {
+    console.log(req.body);
+    if (err) {
+      console.log(err);
+    }
+  });
+  res.redirect('/');
 });
 
 const port = 3000;
